320923582b0a4cb0ca02d49d2159d6db4df24c51
Matthew <faceincake@gmail.com>
2019-01-17 16:29:05 -0500

    Revert "Got rid of the temp commandgroup"
    This reverts commit 841d9966f49a47ddab7db5bf21f4af338581c2a0.

---
+++ src/main/cpp/Commands/Auto_TurnSeveralTimes.cpp
@@ -0,0 +1,38 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+#include "Commands/Auto_TurnSeveralTimes.h"
+
+Auto_TurnSeveralTimes::Auto_TurnSeveralTimes() {
+	// Add Commands here:
+	// e.g. AddSequential(new Command1());
+	//      AddSequential(new Command2());
+	// these will run in order.
+
+	// To run multiple commands at the same time,
+	// use AddParallel()
+	// e.g. AddParallel(new Command1());
+	//      AddSequential(new Command2());
+	// Command1 and Command2 will run in parallel.
+
+	// A command group will require all of the subsystems that each member
+	// would require.
+	// e.g. if Command1 requires chassis, and Command2 requires arm,
+	// a CommandGroup containing them would require both the chassis and the
+	// arm.
+
+	// Right Face
+	AddSequential(new TurnFromAngle(90)) ;
+
+	// Turn 135 deg left
+	AddSequential(new TurnFromAngle(225)) ;
+
+	// Turn 45 right
+	AddSequential(new TurnFromAngle(45)) ;
+
+	return ;
+}
