979eb90535ca056c342f2f6e5ac20a139a1c1e90
Evan Pratten <ewpratten@gmail.com>
2019-01-21 09:50:32 -0500

    updated subsystem

---
+++ src/main/include/Subsystems/ClimbSonic.h
@@ -0,0 +1,25 @@
+//! A subsystem that interfaces with the drivebase on the robot
+#ifndef _CLIMBSONIC_HG_
+#define _CLIMBSONIC_HG_
+
+#include <frc/commands/Subsystem.h>
+#include <Ultrasonic.h>
+#include "RobotMap.h"
+
+class ClimbSonic : public frc::Subsystem {
+ public:
+  ClimbSonic(); //!< Class constructor
+  void InitDefaultCommand() override; //!< Initalizes the default command for this subsystem (DriveWithJoystick)
+	
+	/**
+	 * Get the distance reported by the sensor
+	 *
+	 * @return Distance in MM
+	 */
+  double GetDistance();
+
+ private:
+  Ultrasonic *sensor //!< The ultrasonic sensor
+};
+
+#endif // _CLIMBSONIC_HG_
