84a8fe81b469f9309be71a163ad0d2291a722695
Evan Pratten <ewpratten@gmail.com>
2019-02-12 15:04:25 -0500

    moved commands to autoinit

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -120,20 +120,8 @@ void Robot::AutonomousInit() {
   // if (m_autonomousCommand != nullptr) {
   //   m_autonomousCommand->Start();
   // }
-}
 
-void Robot::AutonomousPeriodic() { frc::Scheduler::GetInstance()->Run(); }
-
-void Robot::TeleopInit() {
-  // This makes sure that the autonomous stops running when
-  // teleop starts running. If you want the autonomous to
-  // continue until interrupted by another command, remove
-  // this line or comment it out.
-  // if (m_autonomousCommand != nullptr) {
-  //   m_autonomousCommand->Cancel();
-  //   m_autonomousCommand = nullptr;
-  // }
-
+  
   if (this->pTriggerDrive != nullptr) {
 		this->pTriggerDrive->Start();
 	}
@@ -152,6 +140,20 @@ void Robot::TeleopInit() {
   if (this->pControlCompressor != nullptr) {
 		this->pControlCompressor->Start();
 	}
+}
+
+void Robot::AutonomousPeriodic() { frc::Scheduler::GetInstance()->Run(); }
+
+void Robot::TeleopInit() {
+  // This makes sure that the autonomous stops running when
+  // teleop starts running. If you want the autonomous to
+  // continue until interrupted by another command, remove
+  // this line or comment it out.
+  // if (m_autonomousCommand != nullptr) {
+  //   m_autonomousCommand->Cancel();
+  //   m_autonomousCommand = nullptr;
+  // }
+
 }
 
 void Robot::TeleopPeriodic() { frc::Scheduler::GetInstance()->Run(); }
