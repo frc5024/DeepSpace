74047a3c5a22bf59cf50b0f33e067e9c883224e1
Matthew <faceincake@gmail.com>
2019-02-14 22:18:41 -0500

    Non specefic new magic numbers added to robotmap. Added the NavX to the vendor dep

--- src/main/include/RobotMap.h
+++ src/main/include/RobotMap.h
@@ -10,6 +10,22 @@
 
 // Whenever possible, Macros and definitions are used in order to preserve memory
 
+// Robot Specifications (for the main bot)
+#define ROBOT_CYCLE_TIME			0.02	//!< seconds
+#define ROBOT_WHEEL_DIAMETER		19.25	//!< centimetres
+#define ROBOT_WHEEL_RADIUS			9.625	//!< centimetres
+#define ROBOT_WHEEL_CIRCUMFERENCE	60.48	//!< centimetres
+
+// Radial Turning Constants
+#define RADIAL_LENGTH				23		//!< centimetres, distance from vision camera to front of robot
+#define RADIAL_WHEELBASE_WIDTH_2	24.91	//!< centimetres, distance from centre of robot to wheels
+#define RADIAL_RPM					456		//!< RPM of both mini-bot and main-bot's motors
+
+// Encoders
+#define ENCODER_TICKS_PER_REVOLUTION	1440	//!< Also known as the resolution, as this is how precise we can be within one rotation
+#define ENCODER_SLOT_INDEX				0		//!< 1 if we do auxillery stuff. we don't
+#define ENCODER_TIMEOUT					100		//1< milliseconds, program will wait AT MOST this long for the encoders to reset before continuing, this 
+
 // Camera mappings
 #define CAMERASERVER_DRIVER_CAMERA 1
 #define CAMERASERVER_VISION_CAMERA 0
